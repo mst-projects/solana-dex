dbd2a5404450288895f2fa950cfea0fda27dc36c
mst-projects <masa-t-hub@users.noreply.github.com>
2022-01-30 14:06:00 +0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "eslint-plugin-tsdoc"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  rules: {
+    "tsdoc/syntax": "warn",
+    "@typescript-eslint/no-explicit-any": 0,
+    "object-shorthand": ["error", "always"],
+  },
+};
